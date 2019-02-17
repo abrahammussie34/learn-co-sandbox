e1a3bd178402bce743412537489607a291ba3efc
Jabe Rooney <abrahamsahlzghi@gmail.com>
2019-02-17 16:46:58 +0000

    Automatically backed up by Learn

---
+++ hello_world.rb
@@ -0,0 +1,59 @@
+#greeting = "Hello atlanta fasttrack"
+#puts greeting
+#puts greeting.downcase
+#puts greeting.upcase
+
+#class_name + "february"
+#greeting = "Hello world" + name + "great to meet you!"
+
+
+#def say_message(greeted)
+#  message = "hello " + greeted.to_s + " great to meet you!"
+#  puts message
+#end
+
+#if 6 == 6
+#  puts "math is working"
+#end
+
+#boolean
+#true, false 
+# && , ||
+#if 6 == 7 
+#  puts "call bank"
+#end
+
+
+#10.times do |step|
+#  puts "we are on step" + step.to_s
+#  say_message("Brit")
+  #only do this when step is 7
+#  if step = 7
+#    puts "hello we are on step 7"
+#  x = step
+#end
+#end
+
+#puts " x is " + x.to_s
+
+#say_message("february")
+#say_message("march")
+#month = 4
+#say_message(month)
+#say_message
+#say_message
+
+
+#try to write a program that
+#asks the user for input
+#if the input matches your secret string, print a secret message
+#otherwise, print a normal message
+
+puts "What is the codeword?" 
+input = gets.strip 
+if input == "open" 
+  puts "yay you got it"
+else
+  puts "you got it wronge"
+end  
+
